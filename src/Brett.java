c5d86b96cbca3193da80ff8348b60da7d2ff7fd7
HackerHenri <supahenri@gmail.com>
2021-03-25 20:02:48 +0100

    neue Begriffe

--- src/Brett.java
+++ src/Brett.java
@@ -11,16 +11,18 @@ public class Brett implements BingoBoard {
     private String[] begriffe = {"<html>Ein Model weint", "<html>Es wird gekreischt", "<html>Heidi sagt „Meine Mädchen“",
     		"<html>Jemand spricht unnötig Englisch", "<html>„Es kann nur eine GNTM werden“", "<html>„Ich habe heute leider kein Foto für dich“", 
     		"<html>Jemand stolpert oder verliert einen Schuh beim walk", "<html>Heidi benutzt das Plusquamperfekt", 
-    		"<html>Attitude", "<html>„Nur eine kommt auf das Cover der deutschen Harpers Bazar“", "Die Mädchen werden gefittet", 
-    		"<html>Soulin ist in einen Streit verwickelt", "<html>Linda sagt/macht was unsympatisches", "<html>Fotograf ist unzufrieden",
+    		"<html>Attitude", "<html>„Nur eine kommt auf das Cover der deutschen Harpers Bazar“",
+    		"<html>Soulin ist in einen Streit verwickelt", "<html>Linda sagt/macht was unsympatisches", "<html>Fotograf/Gastjuror ist unzufrieden",
     		"<html>Jemand sagt was Dummes", "<html>Jemand ist neidisch", "<html>Schuhe oder Kleid unbequem oder passen nicht",
-    		"<html>Ein Mädchen wackelt", "<html>Es wird auf Corona Maßnahmen hingewiesen", "<html>Diversity", "<html>Es wird gelästert",
-    		"<html>Lilliana ist ne Zicke", "<html>Ein Mädchen vermisst Familie/Freund", "<html>Es wird etwas geblurrt/zensiert",
-    		"<html>„Das gab es noch nie bei GNTM!“", "<html>„Wer hat das Zeug GNTM zu werden?“", "<html>„Das allein reicht leider nicht“",
-    		"<html>Corona Handshake", "<html>Schleichwerbung", "<html>Klamotten in denen man nicht laufen kann", "<html>Gastjuror, den keiner kennt",
+    		"<html>Ein Mädchen wackelt", "<html>Diversity", "<html>Es wird gelästert",
+    		"<html>Ein Mädchen vermisst Familie/Freund", "<html>Es wird etwas geblurrt/zensiert",
+    		"<html>Schleichwerbung", "<html>Klamotten in denen man nicht laufen kann", "<html>Gastjuror, den keiner kennt",
     		"<html>Jemand hat Beute von einer Großwildjagd an", "<html>Jemand sitzt im Gammellook auf dem Sofa", "<html>Der Fotograf oder Designer ist schwul",
-    		"<html>Ana sagt was religiöses", "<html>Es wird Freund oder Familie gezeigt", "<html>Personality", "Arroganz", "<html>Heidi sagt dummen Spruch/Sprichwort",
-    		"<html>Heidi ist giga unlustig", "<html>„Wer ist das?!“", "<html>Werbung vor der Entscheidung"};
+    		"<html>Ana sagt was religiöses", "<html>Es wird Freund oder Familie gezeigt", "<html>Personality", "<html>Arroganz", 
+    		"<html>Heidi sagt dummen Spruch/Sprichwort","<html>Heidi ist giga unlustig", "<html>„Wer ist das?!“", "<html>Werbung vor der Entscheidung", 
+    		"<html>Bescheuerte Musik wird eingespielt", "<html>Hässliche Klamotten bei Shoot/Walk", 
+    		"<html>Jemand hat eine hässliche Frisur", "<html>Jemand isst was (Gyros Teller)", "<html>'Cause I just don't know where to go",
+    		"<html>Gastjuror stimmt Heidi zu / kriecht ihr in den Arsch"};
     
     public Brett() {
     	feld = new int[6][6];													  // neues Array
